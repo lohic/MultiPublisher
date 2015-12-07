b1238367cfdf1a9c54d3f15fd7cff6aec9cfddf4
Loïc Horellou <hello@loichorellou.net>
2015-12-07 21:07:01 +0100

    clean up tinymce button script

--- inc/tinymce_js/mp_tinymce_buttons.js
+++ inc/tinymce_js/mp_tinymce_buttons.js
@@ -2,31 +2,6 @@
      /* Register the buttons */
      tinymce.create('tinymce.plugins.mp_button_script', {
           init : function(ed, url) {
-               /**
-               * Inserts shortcode content
-               */
-               // ed.addButton( 'button_eek', {
-               //      title : 'Insert shortcode',
-               //      image : '../wp-includes/images/smilies/icon_eek.gif',
-               //      onclick : function() {
-               //           ed.selection.setContent('[myshortcode]');
-               //      }
-               // });
-               /**
-               * Adds HTML tag to selected content
-               */
-               // ed.addButton( 'button_green', {
-               //      title : 'Add span',
-               //      image : url + '/img/btn_tab.png',
-               //      cmd: 'button_green_cmd'
-               // });
-               // ed.addCommand( 'button_green_cmd', function() {
-               //      var selected_text = ed.selection.getContent();
-               //      var return_text = '';
-               //      return_text = '<h1>' + selected_text + '</h1>';
-               //      ed.execCommand('mceInsertContent', 0, return_text);
-               // });
-
 
                /**
                 * [title description]
@@ -85,57 +60,9 @@
 							}
 						}
                     ]
-                    //icon : 'icon dashicons-images-alt',
-                    //cmd: 'mp_tab_cmd'
-                    // onclick : function() {
-                    //      //ed.selection.setContent('<table><tr><td></td><td></td></tr><tr><td></td><td></td></tr></table>');
-
-                         
-                    //      ed.windowManager.open({
-                    //         title : 'Ajouter une mosaïque d\'images',
-                    //         url : url + '/views/mp-gallery.html',
-                    //         // width : 320,
-                    //         // height : 240
-                    //      }, {
-                    //         custom_param : 1,
-                    //         plugin_url : url,
-                    //         wp: wp,
-                    //         ed: ed
-                    //      });
-                    // }
-                    
-
-
-
-
-                    //Extend the wp.media object
-                    // http://stackoverflow.com/questions/26263597/open-access-wp-media-library-from-tinymce-plugin-popup-window
-                    // onclick : function() {
-                    //   custom_uploader = wp.media.frames.file_frame = wp.media({
-                    //       title: 'Choose Image',
-                    //       button: {
-                    //           text: 'Choose Image'
-                    //       },
-                    //       multiple: false
-                    //   });
-
-                    //   //When a file is selected, grab the URL and set it as the text field's value
-                    //   custom_uploader.on('select', function() {
-                    //       var attachment = custom_uploader.state().get('selection').first().toJSON();
-                    //       $upload_button.siblings('input[type="text"]').val(attachment.url);
-                    //   });
-
-                    //   //Open the uploader dialog
-                    //   custom_uploader.open();
-                    // }
-
+     
                });
-               // ed.addCommand( 'mp_tab_cmd', function() {
-               //      var selected_text = ed.selection.getContent();
-               //      var return_text = '';
-               //      return_text = '<h1>' + selected_text + '</h1>';
-               //      ed.execCommand('mceInsertContent', 0, return_text);
-               // });
+
           },
           createControl : function(n, cm) {
                return null;

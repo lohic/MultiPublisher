f6c83fef7e62d1aaa06c320d4958be24358183b8
armansansd <contact@armansansd.net>
2016-02-18 14:51:59 +0100

    creation d'un fichier backUp, test de la fonction mp.gallery

--- inc/tinymce_js/mp_gallery_plugin.backUp.js
+++ inc/tinymce_js/mp_gallery_plugin.backUp.js
@@ -1,20 +1,27 @@
 jQuery(document).ready(function($) {
 
+
 (function() {
 
-	//console.log("mp_gallery_plugin.js loaded");
+	console.log("mp_gallery_plugin.js loaded");
+
+	//-------------------------------------------
+	//------------- xrefSHORTCODE ---------------
+	//-------------------------------------------
 
 	tinymce.create('tinymce.plugins.mp_gallery', {
 
 		init : function(ed, url) {
-
 			var t = this;
-				console.log(t);
+
 			t.url = url;
+
+			//console.log('MCE plugin gallery for multipublisher created');
+
+
 			//replace shortcode before editor content set
 			ed.onBeforeSetContent.add(function(ed, o) {
 				o.content = t._do_spot(o.content);
-				// o.content >> table gallery
 			});
 
 			//replace shortcode as its inserted into editor (which uses the exec command)
@@ -30,7 +37,9 @@ jQuery(document).ready(function($) {
 			});
 
 			ed.onDblClick.add(function(ed, e) {
-					console.log( e.target.className );
+			    //ed.selection.select(e.target);
+
+			    //console.log( e.target.className );
 
 			    if( e.target.classList.contains('mp_gallery')===true ){
 
@@ -44,6 +53,15 @@ jQuery(document).ready(function($) {
 
 					// console.log(shortcode_obj);
 
+					/*var data = {
+				        'action'	: 'dialog_edit_xref',
+				        'xref_id'	: xref.data('id'),//shortcode_obj.id,
+				        'xref_href'	: xref.data('href'),//shortcode_obj.href,
+				        'xref_txt'	: xref.text(),//shortcode_obj.txt,
+				        'parent_id'   : $("#main_parent_id_field").val()
+				    };*/
+
+				    //$.post(ajaxurl, data, function(response) {
 
 				    response = "RIEN";
 
@@ -75,6 +93,8 @@ jQuery(document).ready(function($) {
 				            	$(".mp-gallery-dialog").remove();
 				            }
 				        }).dialog('open');
+				    //});
+
 			    }
 			});
 
@@ -88,6 +108,78 @@ jQuery(document).ready(function($) {
 
 				var shortCodeObj = shortCode2Obj(b);
 
+				// console.log('gallery shortCodeObj',shortCodeObj);
+
+				// var render = $('<table>')
+				// .addClass('mp_gallery')
+				// .addClass('mceItem')
+				// //.text( decodeURI(shortCodeObj.txt) )
+				// .attr("data-param",shortCodeObj.param)
+				// .attr("data-type",shortCodeObj.type)
+				// .attr("data-ids",shortCodeObj.ids)
+				// //.attr("data-href",shortCodeObj.href);
+				// .append(
+				// 	$('<tr>')
+				// 	.append(
+				// 		$('<td>').text( " " )
+				// 	)
+				// 	.append(
+				// 		$('<td>').text( " " )
+				// 	)
+				// )
+				// .append(
+				// 	$('<tr>')
+				// 	.append(
+				// 		$('<td>').text( " " )
+				// 	)
+				// 	.append(
+				// 		$('<td>').text( decodeURI(shortCodeObj.txt.trim() ) )
+				// 	)
+				// );
+
+				// OLD
+				/*var data = {
+	               // 'ID': $("#post_ID").val(),
+	                action 	: 'galleries_get_json',
+	                type 	: shortCodeObj.type,
+	                ids 	: shortCodeObj.ids
+	            };
+
+	            console.log(data, ajaxurl);*/
+
+
+	            // je me demande si je ne vais pas faire en sorte que le json des structures des galleries soit déjà chargé
+	            // afin d'éviter un ajax synchrone qui fige l'execution du code tant qu'il n'est par résolu
+
+	           	// OLD
+	            // http://stackoverflow.com/questions/5316697/jquery-return-data-after-ajax-call-success#5316755
+	            /*function getGalleries(data){
+
+	            	var result="";
+
+	            	$.ajax({
+	            		url 	 : ajaxurl,
+						async 	 : false,
+						data 	 : data,
+						dataType : 'json',
+						success  : function(response) {
+
+			            	result = response;
+
+			                //console.log('galleries_get_json',response, data);
+
+		            	}
+		            });
+
+	            	return result;
+
+	            }*/
+
+	            //console.log("TAB data json", gallerie_data);
+
+	            // OLD
+	            //console.log('galleries_get_json getGalleries', getGalleries(data) );
+	            //var render = $( getGalleries(data)[ shortCodeObj.type ] )
 	            var render = $( gallerie_data.default[ shortCodeObj.type ] )
 				.addClass('mp_gallery')
 				.addClass('mceItem')
@@ -96,12 +188,43 @@ jQuery(document).ready(function($) {
 				.attr("data-ids",shortCodeObj.ids);
 
 
+
+
+				// analyser shortCodeObj.type (tester le premier chiffre à l'aide substr(0, 1) )
+				// charger les data ids, en fonction du nombre d'images de la galerie et les associer aux classes présentes (a,b,c,d)
+				// on peut formater la requete ajax suivante afin qu'elle renvoie de l'html directement
+
+				// var data = {
+	   //             // 'ID': $("#post_ID").val(),
+	   //              action 	: 'galleries_image_get_html', // va appeler la fonction correspondante dans mp.class.php (wp_ajax_galleries_image_get_html ligne 221)
+	   //              id_image: id // à récupérer dans la boucle de test sur la liste shortCodeObj.ids
+	   //          };
+
+				// $.ajax({
+    //         		url 	 : ajaxurl, // on ne touche pas, variable wordpress
+				// 	data 	 : data, // variable data si dessus
+				// 	dataType : 'html', // on va renvoyer de l'html
+				// 	success  : function(response) {
+
+		  //           	result = response;
+
+		  //               //console.log('galleries_get_json',response, data);
+	   //          	}
+		   //          });
+
+
+
+					// console.log(decodeURI(shortCodeObj.txt));
+					// console.log('outerHTML',render.prop('outerHTML'))
+
 				return render.prop('outerHTML');
 
 			});
-
+			console.log('do_spot');
 		},
+
 		_get_spot : function(co) {
+			console.log('get_spot')
 			function getAttr(s, n) {
 				n = new RegExp(n + '=\"([^\"]+)\"', 'g').exec(s);
 				return n ? tinymce.DOM.decode(n[1]) : '';

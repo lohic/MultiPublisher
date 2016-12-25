c09babf29a77c225a15da4b74404d8194ed9d1fb
Loïc Horellou <hello@loichorellou.net>
2016-12-25 17:57:53 +0100

    remove file
    fichiers inutiles

--- inc/open-posts.js
+++ inc/open-posts.js
@@ -1,45 +1,45 @@
 jQuery(document).ready(function($) {
-  // Find posts
-  var $findBox = $('#find-posts'),
-      $found   = $('#find-posts-response'),
-      $findBoxSubmit = $('#find-posts-submit');
+	// Find posts
+	var $findBox = $('#find-posts'),
+	$found   = $('#find-posts-response'),
+	$findBoxSubmit = $('#find-posts-submit');
 
-  // Open
-  $('input.kc-find-post').live('dblclick', function() {
-    $findBox.data('kcTarget', $(this));
-    findPosts.open();
-  });
+	// Open
+	$('input.kc-find-post').live('dblclick', function() {
+		$findBox.data('kcTarget', $(this));
+		findPosts.open();
+	});
 
-  // Insert
-  $findBoxSubmit.click(function(e) {
-    e.preventDefault();
+	// Insert
+	$findBoxSubmit.click(function(e) {
+		e.preventDefault();
 
-    // Be nice!
-    if ( !$findBox.data('kcTarget') )
-      return;
+		// Be nice!
+		if ( !$findBox.data('kcTarget') )
+			return;
 
-    var $selected = $found.find('input:checked');
-    if ( !$selected.length )
-      return false;
+		var $selected = $found.find('input:checked');
+		if ( !$selected.length )
+			return false;
 
-    var $target = $findBox.data('kcTarget'),
-        current = $target.val(),
-        current = current === '' ? [] : current.split(','),
-        newID   = $selected.val();
+		var $target = $findBox.data('kcTarget'),
+		current = $target.val(),
+		current = current === '' ? [] : current.split(','),
+		newID   = $selected.val();
 
-    if ( $.inArray(newID, current) < 0 ) {
-      current.push(newID);
-      $target.val( current.join(',') );
-    }
-  });
+		if ( $.inArray(newID, current) < 0 ) {
+			current.push(newID);
+			$target.val( current.join(',') );
+		}
+	});
 
-  // Double click on the radios
-  $('input[name="found_post_id"]', $findBox).live('dblclick', function() {
-    $findBoxSubmit.trigger('click');
-  });
+	// Double click on the radios
+	$('input[name="found_post_id"]', $findBox).live('dblclick', function() {
+		$findBoxSubmit.trigger('click');
+	});
 
-  // Close
-  $( '#find-posts-close' ).click(function() {
-    $findBox.removeData('kcTarget');
-  });
+	// Close
+	$( '#find-posts-close' ).click(function() {
+		$findBox.removeData('kcTarget');
+	});
 });
